9a613b661a6fd9794c4cb4fae8d5a2ad81cc60f8
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-11-08 23:02:05 +1100

    create item data static class to hold all item info

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemData 
+{
+    public static List<string> shelfItems = new List<string>()
+    {
+        "Beans",
+        "Noodles",
+        "Tomato Sauce",
+        "Flour",
+        "Pasta",
+        "Chips",
+        "Salt",
+        "Pepper",
+        "Mustard",
+        "Peanuts",
+        "Cola",
+        "Lemonade",
+        "Batteries",
+        "Light Globe",
+        "Cat Food",
+        "Dog Food"
+    };
+}
