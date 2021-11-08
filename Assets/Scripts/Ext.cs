ccf6c66e7da1202388e3e6b23a0e2d2607a2444d
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-11-08 23:00:55 +1100

    create Extenstion class to shuffle any list type

---
+++ Assets/Scripts/Ext.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ext : MonoBehaviour
+{
+    public static List<T> Shuffle<T>(List<T> _list)
+    {
+        for (int i = 0; i < _list.Count; i++)
+        {
+            T temp = _list[i];
+            int randomIndex = UnityEngine.Random.Range(i, _list.Count);
+            _list[i] = _list[randomIndex];
+            _list[randomIndex] = temp;
+        }
+        return _list;
+    }
+}
