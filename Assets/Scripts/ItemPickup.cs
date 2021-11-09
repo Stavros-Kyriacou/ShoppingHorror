7f0e6fb125f541fa8fe4c7ae27b03161756cb420
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-11-09 22:21:59 +1100

    create item pickup script

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Camera cam;
+    public LayerMask layerMask;
+    public TextMeshProUGUI itemNameText;
+
+    private void DrawItemToPickupRay()
+    {
+        
+    }
+}
